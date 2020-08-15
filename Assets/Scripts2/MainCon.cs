f4f8eb7800ac8b58b52fd9aa75bab087869dc4c5
Blake Howes <blakehowes1@gmail.com>
2020-08-15 17:25:44 +1200

    Camera Rework
    Middle mouse to pan, wheel to zoom, click on basic tower to orbit.

--- Assets/Scripts2/MainCon.cs
+++ Assets/Scripts2/MainCon.cs
@@ -7,12 +7,38 @@ public class MainCon : MonoBehaviour
     [SerializeField]
     public bool SelectionActive;
     [SerializeField]
-    private GameObject TowerSelected;
+    private GameObject TowerSelectedForPlacement;
+    [SerializeField]
+    private GameObject CameraTarget;
+
+    private bool OrbitMode = false;
     private bool SpriteisOff = true;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (SelectionActive == false)
+            {
+                LayerMask layermask = LayerMask.GetMask("Tower");
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, layermask))
+                {
+                    //SetZoom and position
+                    Debug.Log("TowerHit");
+                    OrbitMode = true;
+                    CameraTarget.transform.position = hit.transform.position;
+                    Camera.main.GetComponent<CameraCon>().SwitchToOrbit();
+                }
+                else
+                {
+                    if (OrbitMode == true)
+                    {
+                        Camera.main.GetComponent<CameraCon>().SwitchToPan();
+                    }
+                }
+            }
+
             if (SelectionActive == true)
             {
                 LayerMask layermask = LayerMask.GetMask("Wall");
@@ -20,18 +46,13 @@ public class MainCon : MonoBehaviour
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray, out hit, Mathf.Infinity, layermask))
                 {
-                    Instantiate(TowerSelected, hit.transform.position, Quaternion.identity);
+                    Instantiate(TowerSelectedForPlacement, hit.transform.position, Quaternion.identity);
                     SelectionActive = false;
                 }
                 else
                 {
                     SelectionActive = false;
                 }
-            }
-
-            if(SelectionActive == false)
-            {
-                //Highlight Tower
             }
         }
 
@@ -55,6 +76,6 @@ public class MainCon : MonoBehaviour
 
     public void AddObjectToSelection(GameObject TowerPrefab)
     {
-        TowerSelected = TowerPrefab;
+        TowerSelectedForPlacement = TowerPrefab;
     }
 }
