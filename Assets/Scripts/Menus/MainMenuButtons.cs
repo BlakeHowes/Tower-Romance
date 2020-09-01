82cb6f1b15008ee0f142aa6cb3343bdf5714e10b
Al <theonlypurplecat@gmail.com>
2020-09-01 15:14:48 +1200

    TitleWithWorkingButtonsAdded

---
+++ Assets/Scripts/Menus/MainMenuButtons.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject MainTitle;
+
+
+    [SerializeField]
+    private GameObject ControlInstructions;
+
+    [SerializeField]
+    private GameObject AboutCredits;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainTitle.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void InstructionsButton()
+    {
+
+
+        InstructionsON();
+
+
+    }
+
+
+    private void InstructionsON()
+    {
+        MainTitle.SetActive(false);
+        ControlInstructions.SetActive(true);
+    }
+
+    public void ExitInstructions()
+    {
+        InstructionsOFF();
+    }
+    
+    private void InstructionsOFF()
+    {
+        ControlInstructions.SetActive(false);
+        MainTitle.SetActive(true);
+    }
+
+
+
+
+    public void CreditsButton()
+    {
+        CreditsON();
+    }
+
+    private void CreditsON()
+    {
+        MainTitle.SetActive(false);
+        AboutCredits.SetActive(true);
+    }
+
+    public void ExitCredits()
+    {
+        CreditsOFF();
+    }
+
+    private void CreditsOFF()
+    {
+        
+        AboutCredits.SetActive(false);
+        MainTitle.SetActive(true);
+    }
+
+
+
+
+    // Quit Application
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
